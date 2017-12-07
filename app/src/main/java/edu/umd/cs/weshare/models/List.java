73f0797e2da0b1af4674fa5015f88759b7fa0223
Elise <egreen96@terpmail.umd.edu>
2017-12-07 11:40:54 -0500

    list organized by category and alphabetical

--- app/src/main/java/edu/umd/cs/weshare/models/List.java
+++ app/src/main/java/edu/umd/cs/weshare/models/List.java
@@ -23,10 +23,31 @@ public class List {
 
   public void addItem(GroceryItem item) {
     int index = itemsArray.indexOf(item);
+    Category c = item.getCatergory();
     if(index < 0) {
-      itemsArray.add(new GroceryItem(item));
+      int ii = findSpot(c,item.getName());
+      itemsArray.add(ii, new GroceryItem(item));
     } else {
       itemsArray.get(index).incQuantity();
     }
+  }
+
+  private int findSpot(Category c, String n) {
+    boolean foundCategory = false;
+    for (int i = 0; i < itemsArray.size(); i++) {
+      // This spot currently has correct category
+      if (itemsArray.get(i).getCatergory() == c) {
+        foundCategory = true;
+        String name = itemsArray.get(i).getName();
+        // Would putting it here be in alphabetical order?
+        if (name.compareTo(n) > 0) {
+          return i;
+        }
+      } else if (foundCategory) {
+        return i;
+      }
+
+    }
+    return itemsArray.size();
   }
 }
